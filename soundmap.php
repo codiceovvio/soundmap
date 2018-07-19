d340dd8d4ac1a56adb96439a99f1e48cc3f0a29d
Codice Ovvio <codiceovvio@gmail.com>
2018-07-19 18:58:03 +0200

    Add plugin costants in soundmap.php

--- soundmap.php
+++ soundmap.php
@@ -36,6 +36,8 @@ if ( ! defined( 'WPINC' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define( 'SOUNDMAP_VERSION', '1.0.0' );
+define( 'SOUNDMAP_BASENAME', plugin_basename( __FILE__ ) );
+define( 'SOUNDMAP_PATH', plugin_dir_path( __FILE__ ) );
 
 /**
  * The code that runs during plugin activation.
