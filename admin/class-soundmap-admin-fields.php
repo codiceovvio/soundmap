97e65474c19e23bba5a61a987d56d7827a6d9cfc
Codice Ovvio <codiceovvio@gmail.com>
2018-07-31 00:16:06 +0200

    Add correct map admin settings fields

--- admin/class-soundmap-admin-fields.php
+++ admin/class-soundmap-admin-fields.php
@@ -23,7 +23,7 @@ class Soundmap_Admin_Fields {
 	/**
 	 * The options for this plugin.
 	 *
-	 * @since    0.1.0
+	 * @since    0.1.1
 	 * @access   private
 	 * @var      array    $version    The options page for this plugin.
 	 */
@@ -54,7 +54,7 @@ class Soundmap_Admin_Fields {
 		$args = [];
 
 		/**
-		 * Registers main options page menu item and form.
+		 * Register map tab options page settings.
 		 */
 		$args['map_tab'] = [
 			'id'           => $this->soundmap . '-map-settings',
@@ -62,37 +62,43 @@ class Soundmap_Admin_Fields {
 			'menu_title'   => 'Soundmap',
 			'object_types' => ['options-page'],
 			'parent_slug'  => 'options-general.php',
-			'option_key'   => 'soundmap_map_settings',
-			'tab_group'    => 'soundmap_map_settings',
+			'option_key'   => $this->soundmap . '_map_settings',
+			'tab_group'    => $this->soundmap . '_map_settings',
 			'tab_title'    => 'Map Settings',
+			'save_button'  => esc_html__( 'Save Sound Map position', 'soundmap' ),
+			'message_cb'   => __CLASS__ . '::soundmap_options_page_message_callback',
 		];
 
 		/**
-		 * Registers secondary options page, and set main item as parent.
+		 * Register extra tab options page settings.
 		 */
-		$args['audio_tab'] = [
-			'id'           => $this->soundmap . '-audio-settings',
+		$args['extra_tab'] = [
+			'id'           => $this->soundmap . '-extra-settings',
 			'title'        => 'Soundmap Options',
-			'menu_title'   => 'Audio Settings',
+			'menu_title'   => 'Extra Settings',
 			'object_types' => ['options-page'],
-			'option_key'   => 'soundmap_secondary_options',
-			'parent_slug'  => 'soundmap_map_settings',
-			'tab_group'    => 'soundmap_map_settings',
-			'tab_title'    => 'Audio Settings',
+			'parent_slug'  => $this->soundmap . '_map_settings',
+			'option_key'   => $this->soundmap . '_extra_settings',
+			'tab_group'    => $this->soundmap . '_map_settings',
+			'tab_title'    => 'Extra Settings',
+			'save_button'  => esc_html__( 'Save Sound Map extra settings', 'soundmap' ),
+			'message_cb'   => __CLASS__ . '::soundmap_options_page_message_callback',
 		];
 
 		/**
-		 * Registers tertiary options page, and set main item as parent.
+		 * Register layout tab options page settings.
 		 */
 		$args['layout_tab'] = [
 			'id'           => $this->soundmap . '-layout-settings',
 			'title'        => 'Soundmap Options',
 			'menu_title'   => 'Layout Settings',
 			'object_types' => ['options-page'],
-			'option_key'   => 'soundmap_tertiary_options',
-			'parent_slug'  => 'soundmap_map_settings',
-			'tab_group'    => 'soundmap_map_settings',
+			'parent_slug'  => $this->soundmap . '_map_settings',
+			'option_key'   => $this->soundmap . '_layout_settings',
+			'tab_group'    => $this->soundmap . '_map_settings',
 			'tab_title'    => 'Layout Settings',
+			'save_button'  => esc_html__( 'Save Sound Map layout settings', 'soundmap' ),
+			'message_cb'   => __CLASS__ . '::soundmap_options_page_message_callback',
 		];
 
 		$this->soundmap_options = $args;
@@ -100,123 +106,102 @@ class Soundmap_Admin_Fields {
 	}
 
 	/**
-	 * Hook in and register a metabox to handle a theme options page and adds a menu item.
+	 * Register a metabox to handle soundmap options page and adds a menu item.
 	 */
 	public function register_options_metabox( $soundmap_options ) {
 
 		$soundmap_options = $this->soundmap_options;
 
-		// setup registered tabs
+
+		// Setup registered tabs
 		$map_tab_options = new_cmb2_box( $soundmap_options['map_tab'] );
-		$audio_tab_options = new_cmb2_box( $soundmap_options['audio_tab'] );
+		$extra_tab_options = new_cmb2_box( $soundmap_options['extra_tab'] );
 		$layout_tab_options = new_cmb2_box( $soundmap_options['layout_tab'] );
 
 		/**
 		 * First Tab: Map settings.
 		 */
 		$map_tab_options->add_field( array(
-			'name'    => esc_html__( 'Map Initial Settings', 'soundmap' ),
+			'desc' => esc_html__( 'Search by entering a location or an address', 'soundmap' ),
+			'id'   => $this->soundmap . '_settings_addr',
+			'type' => 'text_medium',
+			'classes'    => 'thin',
+		) );
+		$map_tab_options->add_field( array(
+			'title'    => esc_html__( 'Map Initial Settings', 'soundmap' ),
 			'desc'    => esc_html__( 'This is the default view settings when the main map is first loaded.', 'soundmap' ),
 			'id'      => $this->soundmap . '_map_div',
 			'type'    => 'text',
 			'render_row_cb' => __CLASS__ . '::soundmap_render_map_div',
 		) );
+		$map_tab_options->add_field( array(
+			'id'   => $this->soundmap . '_settings_lat',
+			'desc' => esc_html__( 'Latitude', 'soundmap' ),
+			'type' => 'text_small',
+			'classes'    => 'alignleft thin',
+		) );
+		$map_tab_options->add_field( array(
+			'id'   => $this->soundmap . '_settings_lng',
+			'desc' => esc_html__( 'Longitude', 'soundmap' ),
+			'type' => 'text_small',
+			'classes'    => 'alignleft thin',
+		) );
+		$map_tab_options->add_field( array(
+			'id'   => $this->soundmap . '_settings_zoom',
+			'desc' => esc_html__( 'Zoom', 'soundmap' ),
+			'type' => 'text_small',
+			'classes'    => 'thin',
+		) );
 
 		/**
-		* Second Tab: Audio settings.
+		* Second Tab: Extra settings.
 		*/
-		$audio_tab_options->add_field( array(
-			'name'    => 'Test Radio',
-			'desc'    => 'field description (optional)',
-			'id'      => 'radio',
-			'type'    => 'radio',
-			'options' => array(
-				'option1' => 'Option One',
-				'option2' => 'Option Two',
-				'option3' => 'Option Three',
-			),
+		$extra_tab_options->add_field( array(
+			$gmaps_api_help = 'Google API key. To get an API key visit google.com',
+			'desc' => esc_html__( $gmaps_api_help, 'soundmap' ),
+			'id'   => $this->soundmap . '_google_api_key',
+			'type' => 'text_medium',
+			'classes'    => 'thin',
 		) );
 
 		/**
 		* Third Tab: Layout settings.
 		*/
 		$layout_tab_options->add_field( array(
-			'name'    => 'Site Background Color',
-			'desc'    => 'field description (optional)',
-			'id'      => 'bg_color',
-			'type'    => 'colorpicker',
-			'default' => '#ffffff',
+			'name'              => esc_html__( 'Frontend map style layer:', 'soundmap' ),
+			'desc'              => esc_html__( 'Select the default map layer', 'soundmap' ),
+			'id'                => $this->soundmap. '_layer_select',
+			'type'              => 'select',
+			'show_option_none'  => false,
+			'options'           => array(
+				'osmmapnik'        => 'OpenStreetMap Mapnik',
+				'osmblackandwhite' => 'OpenStreetMap Black&White',
+				'esriworldimagery' => 'EsriWorld Imagery',
+				'opentopomap'      => 'Open Topo Map',
+			),
 		) );
 		$layout_tab_options->add_field( array(
-			'name' => 'Test Text Area for Code',
-			'desc' => 'field description (optional)',
+			'name'    => 'Marker base Color',
+			'desc' => esc_html__( 'field description (optional)', 'soundmap' ),
+			'id'      => $this->soundmap. 'marker_color',
+			'type'    => 'colorpicker',
+			'default' => '#d33',
+		) );
+		$layout_tab_options->add_field( array(
+			'name' => 'Extra CSS styles',
+			'desc' => esc_html__( 'You can add some extra CSS here to change the frontend map styles', 'soundmap' ),
 			'id'   => 'textarea_code',
 			'type' => 'textarea_code',
 		) );
 	}
 
 	/**
-	 * A CMB2 options-page display callback override which adds tab navigation among
-	 * CMB2 options pages which share this same display callback.
-	 *
-	 * @param CMB2_Options_Hookup $cmb_options The CMB2_Options_Hookup object.
-	 */
-	public function soundmap_options_display_with_tabs( $cmb_options ) {
-
-		$tabs = $this->soundmap_options_page_tabs( $cmb_options );
-
-		?>
-
-		<div class="wrap cmb2-options-page option-<?php echo $cmb_options->option_key; ?>">
-			<?php if ( get_admin_page_title() ) : ?>
-				<h2><?php echo wp_kses_post( get_admin_page_title() ); ?></h2>
-			<?php endif; ?>
-			<h2 class="nav-tab-wrapper">
-				<?php foreach ( $tabs as $option_key => $tab_title ) : ?>
-					<a class="nav-tab<?php if ( isset( $_GET['page'] ) && $option_key === $_GET['page'] ) : ?> nav-tab-active<?php endif; ?>" href="<?php menu_page_url( $option_key ); ?>"><?php echo wp_kses_post( $tab_title ); ?></a>
-				<?php endforeach; ?>
-			</h2>
-			<form class="cmb-form" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="POST" id="<?php echo $cmb_options->cmb->cmb_id; ?>" enctype="multipart/form-data" encoding="multipart/form-data">
-				<input type="hidden" name="action" value="<?php echo esc_attr( $cmb_options->option_key ); ?>">
-				<?php $cmb_options->options_page_metabox(); ?>
-				<?php submit_button( esc_attr( $cmb_options->cmb->prop( 'save_button' ) ), 'primary', 'submit-cmb' ); ?>
-			</form>
-		</div>
-
-		<?php
-
-	}
-
-	/**
-	 * Gets navigation tabs array for CMB2 options pages which share the given
-	 * display_cb param.
-	 *
-	 * @param CMB2_Options_Hookup $cmb_options The CMB2_Options_Hookup object.
-	 *
-	 * @return array Array of tab information.
-	 */
-	public function soundmap_options_page_tabs( $cmb_options ) {
-
-		$tab_group = $cmb_options->cmb->prop( 'tab_group' );
-		$tabs      = array();
-
-		foreach ( CMB2_Boxes::get_all() as $cmb_id => $cmb ) {
-			if ( $tab_group === $cmb->prop( 'tab_group' ) ) {
-				$tabs[ $cmb->options_page_keys()[0] ] = $cmb->prop( 'tab_title' )
-					? $cmb->prop( 'tab_title' )
-					: $cmb->prop( 'title' );
-			}
-		}
-
-		return $tabs;
-
-	}
-
-	/**
-	 * Manually render a field.
+	 * Render the map field.
 	 *
 	 * @param  array      $field_args Array of field arguments.
 	 * @param  CMB2_Field $field      The field object.
+	 *
+	 * @since    0.1.1
 	 */
 	public static function soundmap_render_map_div( $field_args, $field ) {
 		$classes     = $field->row_classes();
@@ -227,10 +212,56 @@ class Soundmap_Admin_Fields {
 		$description = $field->args( 'description' );
 		?>
 		<div class="cmb-row <?php echo esc_attr( $id ) . ' ' . esc_attr( $classes ); ?>">
-			<p class="description"><?php echo esc_html( $description ); ?></p>
+			<span class="cmb-td cmb2-metabox-description"><?php echo esc_html( $description ); ?></span>
 			<div id="map-settings" class="map-settings"></div>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Callback to define the optionss-saved message.
+	 *
+	 * @param CMB2  $cmb The CMB2 object.
+	 * @param array $args {
+	 *     An array of message arguments
+	 *
+	 *     @type bool   $is_options_page Whether current page is this options page.
+	 *     @type bool   $should_notify   Whether options were saved and we should be notified.
+	 *     @type bool   $is_updated      Whether options were updated with save (or stayed
+	 *                                   the same).
+	 *     @type string $setting         For add_settings_error(), Slug title of the setting
+	 *                                   to which this error applies.
+	 *     @type string $code            For add_settings_error(), Slug-name to identify the error.
+	 *                                   Used as part of 'id' attribute in HTML output.
+	 *     @type string $message         For add_settings_error(), The formatted message text
+	 *                                   to display to the user (will be shown inside styled
+	 *                                   `<div>` and `<p>` tags).
+	 *                                   Will be 'Settings updated.' if $is_updated is true,
+	 *                                   else 'Nothing to update.'
+	 *     @type string $type            For add_settings_error(), Message type,
+	 *                                   controls HTML class.
+	 *                                   Accepts 'error', 'updated', '', 'notice-warning', etc.
+	 *                                   Will be 'updated' if $is_updated is true,
+	 *                                   else 'notice-warning'.
+	 * }
+	 *
+	 * @since    0.1.1
+	 */
+	public static function soundmap_options_page_message_callback( $cmb, $args ) {
+
+		if ( ! empty( $args['should_notify'] ) ) {
+
+			if ( $args['is_updated'] ) {
+
+				// Modify the updated message.
+				$args['message'] = sprintf( esc_html__( '%1$s &mdash; %2$s Updated!', 'soundmap' ),
+					$cmb->prop( 'title' ),
+					$cmb->prop( 'tab_title' )
+				);
+			}
+
+			add_settings_error( $args['setting'], $args['code'], $args['message'], $args['type'] );
+		}
 	}
 
 }
