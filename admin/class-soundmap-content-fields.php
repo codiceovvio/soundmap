60223a504aebfa11490812a90b3afa31a759420b
Codice Ovvio <codiceovvio@gmail.com>
2018-09-04 00:23:01 +0200

    Remove place marker fields and fix comments

--- admin/class-soundmap-content-fields.php
+++ admin/class-soundmap-content-fields.php
@@ -55,7 +55,8 @@ class Soundmap_Content_Fields {
 	}
 
 	/**
-	 * Hook in and add a demo metabox. Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
+	 * Hook in and add a metabox for Sound Marker location map. It must hook
+	 * to the 'cmb2_admin_init' or 'cmb2_init' hook.
 	 */
 	public function sound_marker_map() {
 
@@ -101,7 +102,8 @@ class Soundmap_Content_Fields {
 	}
 
 	/**
-	 * Hook in and add a demo metabox. Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
+	 * Hook in and add a metabox for Sound Marker recording info. It
+	 * must hook to the 'cmb2_admin_init' or 'cmb2_init' hook.
 	 */
 	public function sound_marker_recording() {
 
@@ -153,7 +155,8 @@ class Soundmap_Content_Fields {
 	}
 
 	/**
-	 * Hook in and add a demo metabox. Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
+	 * Hook in and add a metabox for Sound Marker recording details. It
+	 * must hook to the 'cmb2_admin_init' or 'cmb2_init' hook.
 	 */
 	public function sound_marker_details() {
 
@@ -199,101 +202,6 @@ class Soundmap_Content_Fields {
 		) );
 
 	}
-
-
-	/**
-	 * Hook in and add a demo metabox. Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
-	 */
-	public function place_marker_map() {
-
-		$prefix = 'place_marker';
-
-		/**
-		 * Sample metabox to demonstrate each field type included
-		 */
-		$marker_map = new_cmb2_box( array(
-			'id'            => $prefix . '_map',
-			'title'         => esc_html__( 'Place Marker - location', 'soundmap' ),
-			'object_types'  => array( 'place_marker' ), // Post type
-			// Enables CMB2 REST API for this box, and set http method the box is visible in.
-			'show_in_rest' => WP_REST_Server::ALLMETHODS,
-		) );
-		$marker_map->add_field( array(
-			'id'   => $prefix . '_map_div',
-			'desc' => esc_html__( 'click to save a location for this place', 'soundmap' ),
-			'type' => 'text',
-			'render_row_cb' => __CLASS__ . '::soundmap_render_map_div',
-			'show_in_rest' => false,
-		) );
-		$marker_map->add_field( array(
-			'id'   => $prefix . '_lat',
-			'desc' => esc_html__( 'Latitude', 'soundmap' ),
-			'type' => 'text_small',
-			'classes'    => 'alignleft thin',
-		) );
-		$marker_map->add_field( array(
-			'id'   => $prefix . '_lng',
-			'desc' => esc_html__( 'Longitude', 'soundmap' ),
-			'type' => 'text_small',
-			'classes'    => 'alignleft thin',
-		) );
-		$marker_map->add_field( array(
-			'desc' => esc_html__( 'Search by entering a location or an address', 'soundmap' ),
-			'id'   => $prefix . '_addr',
-			'type' => 'text_medium',
-			'classes'    => 'thin',
-			'show_in_rest' => false,
-		) );
-
-	}
-
-
-	/**
-	 * Hook in and add a demo metabox. Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
-	 */
-	public function place_marker_details() {
-
-			$prefix = 'place_marker';
-
-			/**
-			 * Sample metabox to demonstrate each field type included
-			 */
-			$marker_details = new_cmb2_box( array(
-				'id'            => $prefix . '_details',
-				'title'         => esc_html__( 'Place Marker - details', 'soundmap' ),
-				'object_types'  => array( 'place_marker' ), // Post type
-				// Enables CMB2 REST API for this box, and set http method the box is visible in.
-				'show_in_rest' => WP_REST_Server::ALLMETHODS,
-			) );
-			$marker_details->add_field( array(
-				'name' => esc_html__( 'Wiki URL', 'soundmap' ),
-				'desc' => esc_html__( 'field description (optional)', 'soundmap' ),
-				'id'   => $prefix . '_wiki_url',
-				'type' => 'text_url',
-				// 'date_format' => 'Y-m-d',
-			) );
-			$marker_details->add_field( array(
-				'name' => esc_html__( 'Place Info', 'soundmap' ),
-				'desc' => esc_html__( 'field description (optional)', 'soundmap' ),
-				'id'   => $prefix . '_info',
-				'type' => 'textarea',
-				'time_format' => 'H:i', // Set to 24hr format
-			) );
-			$marker_details->add_field( array(
-				'name' => esc_html__( 'Author URL', 'soundmap' ),
-				'desc' => esc_html__( 'field description (optional)', 'soundmap' ),
-				'id'   => $prefix . '_author_url',
-				'type' => 'text_url',
-				'protocols' => array( 'http', 'https', 'mailto' ), // Array of allowed protocols
-			) );
-			$marker_details->add_field( array(
-				'name' => esc_html__( 'Author Email', 'soundmap' ),
-				'desc' => esc_html__( 'field description (optional)', 'soundmap' ),
-				'id'   => $prefix . '_author_email',
-				'type' => 'text_email',
-			) );
-
-		}
 
 	/**
 	 * Manually render a field.
